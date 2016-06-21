0c54ccd7f0adbefd070fa345cd473a8e48c41405
DmitryOsterman <osterman.dg@gmail.com>
2016-06-21 20:32:42 +0900

    add JS

---
+++ js/mystyle.js
@@ -0,0 +1,54 @@
+// ========= полная загрузка странички с использованием jQuery: ========
+$(document).ready(function () {
+
+    console.log($('#idSearch').attr('name'));
+
+// ==========   карусель главный покупок  =============================
+//    $(function () {
+//        $(".mainGoods").jCarouselLite({
+//            auto: 4000,
+//            speed: 2000,
+//            visible: 1
+//        });
+//    });
+// ==========   карусель топ покупок  =============================
+    $(function () {
+        $(".topSale").jCarouselLite({
+            auto: 2000,
+            speed: 2000,
+            visible: 5
+        });
+    });
+});
+//============================================================================
+
+
+//=======       изм цвета через JS     ====================================
+//        $('li a').hover(
+//                function () {
+//                    $(this).css('color', 'orange');
+//                },
+//                function () {
+//                    $(this).css('color', '');
+//                }
+//        );
+
+//    ========= эфект исчезновения и появления строки поиска ==============
+//$('.searchContainer a').bind('click', function () {
+//    $('#idSearch').toggle(555);
+//});
+
+//    ========= варианты карусели ==============
+//$(function () {
+//    $(".goods").jCarouselLite({
+//        auto: 2000,
+//        speed: 2000,
+//        visible: 4
+//        btnNext: ".next",
+//        btnPrev: ".prev"
+//    });
+//});
+
+// ==========   тень заголовков     ================
+//    $('h1').dropShadow({left: 2, top: -19, blur: 0});  // Creates new drop shadows
+//    $('h2').dropShadow({left: 2, top: -17, blur: 0});  // Creates new drop shadows
