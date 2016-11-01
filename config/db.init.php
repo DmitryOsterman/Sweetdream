36817617ee39665b554caf131391be304de7178d
Dmitry <osterman.dg@gmail.com>
2016-11-01 19:22:05 +0900

    Update db.init.php

--- config/db.init.php
+++ config/db.init.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', 'sweetdream');
-define('DB_PASSWORD', 'G7vB9bHEHYuFuqK6');
-define('DB_NAME', 'sweetdream');
-define('ADM_USER', 'admin');
-define('ADM_PASSWORD', 'sa83KE61');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+define('ADM_USER', '');
+define('ADM_PASSWORD', '');
